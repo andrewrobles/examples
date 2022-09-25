11b929b680b384a393f7cf8e49dd5dd93a5e5087
Andrew Robles <andrewrobles@verizon.net>
2022-09-25 14:22:36 -0700

    Render square

--- wip/pixel-react /src/App.js
+++ wip/pixel-react /src/App.js
@@ -1,7 +1,16 @@
+import { useRef, useEffect } from 'react'
+
 function App() {
-  return (
-    <div>Basic React</div>
-  );
+  const canvasRef = useRef(null)
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+    context.fillStyle = '#000000'
+    context.fillRect(0, 0, 20, 20)
+  })
+
+  return <canvas ref={canvasRef} />
 }
 
 export default App;
